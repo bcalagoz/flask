430cff01624c23128b19e14cf0ae5a9eaee62173
Burak Can Alagöz <bcalagoz@icloud.com>
2023-01-23 21:41:40 +0300

    GET and POST methods are tried.

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+from constants import all_products
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    return all_products, 200
+
+
+@app.route('/products', methods=['POST'])
+def insert_product():
+    print(request.json)
+    return {'Message': 'Operation completed!'}, 200
 
 
 if __name__ == "__main__":
---
+++ constants.py
@@ -0,0 +1,21 @@
+all_products = {
+    "products": [
+        {
+          "id": '1',
+          "name": 'puma ayakkabı',
+          "price": 2555,
+          "price_unit": "tl",
+          "image_url": "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/309668/04/sv01/fnd/"
+                       "TUR/w/1000/h/1000/fmt/png"
+        },
+        {
+            "id": '2',
+            "name": 'adidas ayakkabı',
+            "price": 5565,
+            "price_unit": "tl",
+            "image_url": "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/309668/04/sv01/fnd/"
+                         "TUR/w/1000/h/1000/fmt/png"
+        }
+
+    ]
+}
